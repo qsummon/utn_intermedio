ca89ba5af274f062dbceed2e9db0c48cd9c61f2d
Marcelo Zurita <mzurita@directvla.com.ar>
2021-09-19 14:36:46 -0300

    first commit

---
+++ utn_intermedio/lab/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from . import views
+
+urlpatterns = [
+    # url('', views.home, name='lab-home'),
+    path('', PostListView.as_view(), name='lab-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-udpate'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('about/', views.about, name='lab-about'),
+]
---
+++ utn_intermedio/utn_intermedio/urls.py
@@ -0,0 +1,31 @@
+"""utn_intermedio URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('lab/', include('lab.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+# from django.urls import include
+from django.conf.urls import include
+from users import views as user_views
+
+urlpatterns = [
+
+    url(r'^admin/', admin.site.urls),
+    url('register/', user_views.register, name='register'),
+    url('profile/', user_views.profile, name='profile'),
+    url('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    url('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    url('', include('lab.urls')),
+]
